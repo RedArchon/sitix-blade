31439792dc417f9127aaaaaa925df2b8d2cc7f00
Logan Collinsworth <logan@lynxtdc.com>
2022-04-07 23:02:22 -0400

    removed route from web.php

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/page-1', function () {
     return view('page-1');
 });
-
-Route::get('/data', function () {
-    $response = Http::withHeaders([
-        'x-rapidapi-host' => 'free-nba.p.rapidapi.com',
-        'x-rapidapi-key' => 'e5d8563997mshcfb030c802400d3p1ec262jsn6864acf2d34a'
-    ])->get('https://free-nba.p.rapidapi.com/teams?page=0');
-
-    return collect(json_decode($response))->paginate(10);
-});
